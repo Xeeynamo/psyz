a4344dd1aef99aece0d1aa98b432d7b45582737e
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-12-14 14:34:31 +0000

    Implement rsin and rcos

--- decomp/src/libgte/geo_00.c
+++ decomp/src/libgte/geo_00.c
@@ -1,6 +1,30 @@
 #include <common.h>
 #include <libgte.h>
 
-INCLUDE_ASM("asm/nonmatchings/libgte/geo_00", rsin);
+int sin_1(int a);
+int rsin(int a) {
+    if (a < 0) {
+        return -sin_1(-a & 0xFFF);
+    } else {
+        return sin_1(a & 0xFFF);
+    }
+}
 
-INCLUDE_ASM("asm/nonmatchings/libgte/geo_00", sin_1);
+extern short rsin_tbl[];
+int sin_1(int a) {
+    if (a <= 0x800) {
+        if (a <= 0x400) {
+            return rsin_tbl[a];
+        } else {
+            return rsin_tbl[0x800 - a];
+        }
+    } else {
+        if (a <= 0xC00) {
+            return -rsin_tbl[a - 0x800];
+        } else {
+            return -rsin_tbl[0x1000 - a];
+        }
+    }
+}
+
+NOP;
--- decomp/src/libgte/geo_01.c
+++ decomp/src/libgte/geo_01.c
@@ -1,4 +1,24 @@
-#include <common.h>
-#include <libgte.h>
+#include "common.h"
 
-INCLUDE_ASM("asm/nonmatchings/libgte/geo_01", rcos);
+extern short rsin_tbl[];
+
+int rcos(int a) {
+    if (a < 0) {
+        a = -a;
+    }
+    a &= 0xFFF;
+
+    if (a <= 0x800) {
+        if (a <= 0x400) {
+            return rsin_tbl[0x400 - a];
+        } else {
+            return -rsin_tbl[a - 0x400];
+        }
+    } else {
+        if (a <= 0xC00) {
+            return -rsin_tbl[0xC00 - a];
+        } else {
+            return rsin_tbl[a - 0xC00];
+        }
+    }
+}
