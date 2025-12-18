1b70b67dd1468a57b199f8aff568487457e45c02
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-12-18 20:46:17 +0000

    windows: disable GL tests on CI

--- psyz/test.bat
+++ psyz/test.bat
@@ -4,6 +4,7 @@ if not exist "build" mkdir build
 cd build
 cmake .. -DCMAKE_BUILD_TYPE=Debug || (cd ..\.. && exit /b 1)
 cmake --build . --config Debug || (cd ..\.. && exit /b 1)
+copy /Y build\sdl\Debug\SDL3.dll Debug\
 Debug\psyz_tests.exe
 set TEST_RESULT=%errorlevel%
 cd ..\..
