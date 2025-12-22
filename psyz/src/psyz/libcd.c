6970ddf181c3bcf0e18f93ca2b65e125f5b2ec2c
Luciano Ciccariello <xeeynamo@hotmail.com>
2025-12-22 17:56:05 +0000

    Add basic CUE parsing

--- psyz/src/psyz/libcd.c
+++ psyz/src/psyz/libcd.c
@@ -3,8 +3,36 @@
 #include <log.h>
 #include <inttypes.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
 
 #define SECTOR_SIZE 2352
+#define MAX_TRACKS 99
+#define MAX_PATH_LEN 512
+
+// Track types
+typedef enum {
+    TRACK_AUDIO,
+    TRACK_MODE1_2352,
+    TRACK_MODE2_2352,
+    TRACK_UNKNOWN
+} TrackType;
+
+// Track entry from CUE file
+typedef struct {
+    char file_path[MAX_PATH_LEN];
+    int start_sector; // MSF sector offset within the file
+    int abs_sector;   // Absolute sector position on the "virtual CD"
+    int track_num;
+    TrackType type;
+    int is_valid;
+} TrackEntry;
+
+// Global CUE data storage
+static TrackEntry g_tracks[MAX_TRACKS];
+static int g_track_count = 0;
+static char g_cue_base_path[MAX_PATH_LEN];
 
 char* CD_comstr[] = {
     "CdlSync",    "CdlNop",
@@ -42,6 +70,238 @@ u_char CD_com = 0;
 static short __padding = 0;
 int DS_active = 0;
 
+// Convert MSF (minute:second:frame) to LBA sector number
+// CD-ROM sectors: 75 frames per second, 60 seconds per minute
+static int msf_to_sector(int minute, int second, int frame) {
+    return minute * 60 * 75 + second * 75 + frame;
+}
+
+// Parse MSF timestamp from string (format: MM:SS:FF)
+static int parse_msf(
+    const char* msf_str, int* minute, int* second, int* frame) {
+    if (sscanf(msf_str, "%d:%d:%d", minute, second, frame) != 3) {
+        return -1;
+    }
+    return 0;
+}
+
+// Trim whitespace from both ends of a string
+static char* str_trim(char* str) {
+    char* end;
+    while (isspace((unsigned char)*str))
+        str++;
+    if (*str == 0)
+        return str;
+    end = str + strlen(str) - 1;
+    while (end > str && isspace((unsigned char)*end))
+        end--;
+    end[1] = '\0';
+    return str;
+}
+
+// Extract quoted string or unquoted token
+static char* extract_token(char* str, char* dest, int max_len) {
+    char* p = str;
+    int len = 0;
+
+    // Skip leading whitespace
+    while (*p && isspace((unsigned char)*p))
+        p++;
+    if (*p == '"') {
+        // Quoted string
+        p++;
+        while (*p && *p != '"' && len < max_len - 1) {
+            dest[len++] = *p++;
+        }
+        if (*p == '"')
+            p++;
+    } else {
+        // Unquoted token (up to space)
+        while (*p && !isspace((unsigned char)*p) && len < max_len - 1) {
+            dest[len++] = *p++;
+        }
+    }
+    dest[len] = '\0';
+    return p;
+}
+
+// Get file size in sectors
+static int get_file_size_in_sectors(const char* file_path) {
+    FILE* fp = fopen(file_path, "rb");
+    if (!fp) {
+        WARNF("Failed to open file for size calculation: %s", file_path);
+        return 0;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    fclose(fp);
+
+    if (file_size < 0) {
+        WARNF("Failed to get file size: %s", file_path);
+        return 0;
+    }
+
+    // Convert bytes to sectors (rounded up)
+    return (file_size + SECTOR_SIZE - 1) / SECTOR_SIZE;
+}
+
+// Parse a CUE file and populate the global track table
+static int parse_cue_file(const char* cue_path) {
+    FILE* fp = fopen(cue_path, "r");
+    if (!fp) {
+        ERRORF("error opening CUE file %s", cue_path);
+        return -1;
+    }
+
+    // Extract base directory from CUE path for resolving relative file paths
+    const char* last_slash = strrchr(cue_path, '/');
+    if (!last_slash) {
+        last_slash = strrchr(cue_path, '\\');
+    }
+    if (last_slash) {
+        int len = last_slash - cue_path + 1;
+        if (len >= MAX_PATH_LEN)
+            len = MAX_PATH_LEN - 1;
+        strncpy(g_cue_base_path, cue_path, len);
+        g_cue_base_path[len] = '\0';
+    } else {
+        g_cue_base_path[0] = '\0';
+    }
+
+    char full_line[1024];
+    char current_file[MAX_PATH_LEN] = {0};
+    char current_file_full_path[MAX_PATH_LEN] = {0};
+    int current_track = -1;
+    int current_file_abs_sector =
+        0; // Absolute sector where current file starts
+
+    g_track_count = 0;
+    while (fgets(full_line, sizeof(full_line), fp)) {
+        char* line = str_trim(full_line);
+        if (strncmp(line, "FILE", 4) == 0) {
+            // Before switching to new file, calculate size of previous file
+            if (current_file[0] != '\0') {
+                int prev_file_size =
+                    get_file_size_in_sectors(current_file_full_path);
+                current_file_abs_sector += prev_file_size;
+            }
+
+            char* p = line + 4;
+            extract_token(p, current_file, MAX_PATH_LEN);
+
+            // Build full path for the new file
+            if (current_file[0] == '/' || current_file[0] == '\\' ||
+                (current_file[0] != '\0' && current_file[1] == ':')) {
+                // Absolute path
+                strncpy(current_file_full_path, current_file, MAX_PATH_LEN - 1);
+            } else {
+                // Relative path - prepend base directory
+                snprintf(current_file_full_path, MAX_PATH_LEN, "%s%s",
+                         g_cue_base_path, current_file);
+            }
+        } else if (strncmp(line, "TRACK", 5) == 0) {
+            int track_num = 1;
+            char type_str[64];
+            if (sscanf(line, "TRACK %d %63s", &track_num, type_str) == 2) {
+                if (track_num < 1 || track_num > MAX_TRACKS) {
+                    ERRORF("Invalid track number: %d", track_num);
+                    continue;
+                }
+
+                current_track = track_num - 1;
+                TrackEntry* track = &g_tracks[current_track];
+                track->track_num = track_num;
+                track->is_valid = 1;
+                track->start_sector = 0; // Will be set by INDEX 01
+                track->abs_sector =
+                    0; // Will be calculated when INDEX 01 is parsed
+
+                // Store file path
+                strncpy(
+                    track->file_path, current_file_full_path, MAX_PATH_LEN - 1);
+
+                if (strcmp(type_str, "AUDIO") == 0) {
+                    track->type = TRACK_AUDIO;
+                } else if (strcmp(type_str, "MODE1/2352") == 0) {
+                    track->type = TRACK_MODE1_2352;
+                } else if (strcmp(type_str, "MODE2/2352") == 0) {
+                    track->type = TRACK_MODE2_2352;
+                } else {
+                    track->type = TRACK_UNKNOWN;
+                    DEBUGF("Unknown track type: %s", type_str);
+                }
+
+                if (track_num > g_track_count) {
+                    g_track_count = track_num;
+                }
+            }
+        } else if (strncmp(line, "INDEX", 5) == 0) {
+            if (current_track < 0) {
+                ERRORF("INDEX found before TRACK");
+                continue;
+            }
+            int index_num;
+            char msf_str[32];
+            if (sscanf(line, "INDEX %d %31s", &index_num, msf_str) == 2) {
+                int minute, second, frame;
+                if (parse_msf(msf_str, &minute, &second, &frame) == 0) {
+                    int sector = msf_to_sector(minute, second, frame);
+                    if (index_num == 1) { // INDEX 01 is the track data
+                        TrackEntry* track = &g_tracks[current_track];
+                        track->start_sector = sector;
+                        track->abs_sector = current_file_abs_sector + sector;
+                        DEBUGF("Track %02d: %s at sector %d", track->track_num,
+                               track->file_path, track->abs_sector);
+                    }
+                }
+            }
+        } else {
+            WARNF("CUE line not recognized: %s", line);
+        }
+    }
+    fclose(fp);
+
+    if (g_track_count == 0) {
+        ERRORF("No valid tracks found in CUE file");
+        return -1;
+    }
+    DEBUGF("Parsed CUE file: %d tracks", g_track_count);
+    return 0;
+}
+
+int Psyz_SetDiskPath(const char* diskPath) {
+    if (!diskPath) {
+        ERRORF("diskPath is NULL");
+        return -1;
+    }
+    g_track_count = 0;
+    memset(g_tracks, 0, sizeof(g_tracks));
+    if (parse_cue_file(diskPath) < 0) {
+        return -1;
+    }
+    DEBUGF("Disk path set: %s", diskPath);
+    return 0;
+}
+static DiskReadCB disk_read_cb = NULL;
+void Psyz_SetDiskCallback(DiskReadCB cb) { disk_read_cb = cb; }
+
+static void psyz_play() {
+    int sector = CdPosToInt(&CD_pos);
+    // TODO based on `pos`, open the right file that was previously read from
+    // CUE
+    // TODO file is always a 44100Hz 2ch audio redbook file, play it
+    NOT_IMPLEMENTED;
+}
+
+static void psyz_stop() { NOT_IMPLEMENTED; }
+
+static void psyz_pause() { NOT_IMPLEMENTED; }
+
+static void psyz_mute() { NOT_IMPLEMENTED; }
+
+static void psyz_demute() { NOT_IMPLEMENTED; }
+
 int CD_init(void) {
     NOT_IMPLEMENTED;
     return 1;
@@ -63,6 +323,64 @@ int CD_ready(int mode, u_char* result) {
 int CD_cw(u8 com, u8* param, u_char* result, s32 arg3) {
     CD_sync(0, 0);
     switch (com) {
+    case CdlSetloc:
+        if (!param) {
+            ERRORF("%s got NULL param", CD_comstr[com]);
+            return -2;
+        }
+        CD_pos = *(CdlLOC*)param;
+        break;
+    case CdlPlay:
+        psyz_play();
+        break;
+    case CdlStop:
+        psyz_stop();
+        break;
+    case CdlPause:
+        psyz_pause();
+        break;
+    case CdlMute:
+        psyz_mute();
+        break;
+    case CdlDemute:
+        psyz_demute();
+        break;
+    case CdlSetmode:
+        if (!param) {
+            ERRORF("%s got NULL param", CD_comstr[com]);
+            return -2;
+        }
+        if (*param & CdlModeDA) {
+            DEBUGF("%s does not support CdlModeDA", CD_comstr[com]);
+        }
+        if (*param & CdlModeAP) {
+            DEBUGF("%s does not support CdlModeAP", CD_comstr[com]);
+        }
+        if (*param & CdlModeRept) {
+            DEBUGF("%s does not support CdlModeRept", CD_comstr[com]);
+        }
+        if (*param & CdlModeSF) {
+            DEBUGF("%s does not support CdlModeSF", CD_comstr[com]);
+        }
+        if (*param & CdlModeSize0) {
+            DEBUGF("%s does not support CdlModeSize0", CD_comstr[com]);
+        }
+        if (*param & CdlModeSize1) {
+            DEBUGF("%s does not support CdlModeSize1", CD_comstr[com]);
+        }
+        if (*param & CdlModeRT) {
+            DEBUGF("%s does not support CdlModeRT", CD_comstr[com]);
+        }
+        if (*param & CdlModeSpeed) {
+            DEBUGF("%s does not support CdlModeSpeed", CD_comstr[com]);
+        }
+        if (*param & CdlModeStream2) {
+            DEBUGF("%s does not support CdlModeStream2", CD_comstr[com]);
+        }
+        if (*param & CdlModeStream) {
+            DEBUGF("%s does not support CdlModeStream", CD_comstr[com]);
+        }
+        break;
     default:
         if (com >= LEN(CD_comstr)) {
             ERRORF("com %X invalid", com);
