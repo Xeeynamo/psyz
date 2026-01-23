00e7c526d39e8974fd0c35706e33f086859c8712
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-01-23 01:49:46 +0000

    decomp: match libapi pad.c and libetc pad.c

--- decomp/src/libetc/pad.c
+++ decomp/src/libetc/pad.c
@@ -1,8 +1,22 @@
 #include <common.h>
 #include <libetc.h>
 
-INCLUDE_ASM("asm/nonmatchings/libetc/pad", PadInit);
+int pad_buf;
+int PadIdentifier;
 
-INCLUDE_ASM("asm/nonmatchings/libetc/pad", PadRead);
+void PAD_dr(int id);
 
-INCLUDE_ASM("asm/nonmatchings/libetc/pad", PadStop);
+void PadInit(int mode) {
+    PadIdentifier = mode;
+    pad_buf = -1;
+    ResetCallback();
+    PAD_init(0x20000001, &pad_buf);
+    ChangeClearPAD(0);
+}
+
+u_long PadRead(int id) {
+    PAD_dr(id);
+    return ~pad_buf;
+}
+
+void PadStop(void) { StopPAD(); }
