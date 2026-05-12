c45843b28be4c7322c6f18ac7dd9c1e792062c68
Luciano Ciccariello <xeeynamo@hotmail.com>
2026-05-12 21:22:26 +0100

    libsnd: SsEnd

--- decomp/src/libsnd/ssend.c
+++ decomp/src/libsnd/ssend.c
@@ -1,4 +1,22 @@
-#include <common.h>
-#include <libsnd.h>
+#include "libsnd_private.h"
 
-INCLUDE_ASM("asm/nonmatchings/libsnd/ssend", SsEnd);
+void SsEnd(void) {
+    if (_snd_seq_tick_env.manual_tick == 0) {
+        _snd_seq_tick_env.unk11 = 0;
+        if (_snd_seq_tick_env.alarm_tick == 0x7F) {
+            return;
+        }
+        EnterCriticalSection();
+        if (_snd_seq_tick_env.vsync_tick) {
+            VSyncCallback(NULL);
+            _snd_seq_tick_env.vsync_tick = 0;
+        } else if (_snd_seq_tick_env.alarm_tick == 0) {
+            InterruptCallback(0, _snd_seq_tick_env.vsync_cb);
+            _snd_seq_tick_env.vsync_cb = NULL;
+        } else {
+            InterruptCallback(6, NULL);
+        }
+        ExitCriticalSection();
+        _snd_seq_tick_env.alarm_tick = 0x7F;
+    }
+}
