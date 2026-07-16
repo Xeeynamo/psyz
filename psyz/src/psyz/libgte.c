26e202723c7208d33d6676e9c0bebace7b813734
Luciano Ciccariello <Xeeynamo@users.noreply.github.com>
2026-07-16 18:30:51 +0100

    gte: implement new Matrix and Color APIs (#98)

--- psyz/src/psyz/libgte.c
+++ psyz/src/psyz/libgte.c
@@ -1095,7 +1095,36 @@ long NormalClip(long sxy0, long sxy1, long sxy2) {
     return MAC0;
 }
 
+void NormalColorCol(SVECTOR* v0, CVECTOR* v1, CVECTOR* v2) {
+    Psyz_GteLdv0(v0);
+    Psyz_GteLdRgb(v1);
+    NCCS(0x1B04084B);
+    Psyz_GteStRgb(v2);
+}
+
+void DpqColor(CVECTOR* v0, long p, CVECTOR* v1) {
+    Psyz_GteLdRgb(v0);
+    IR0 = (short)p;
+    DPCS(0x1000784A);
+    Psyz_GteStRgb(v1);
+}
+
 void RotTrans(SVECTOR* v0, VECTOR* v1, int* flag) { NOT_IMPLEMENTED; }
+
+void ApplyMatrix(MATRIX* m, SVECTOR* v0, VECTOR* v1) {
+    // TODO unoptimized for 32-bit targets
+    v1->vx = ((long long)m->m[0][0] * v0->vx + (long long)m->m[0][1] * v0->vy +
+              (long long)m->m[0][2] * v0->vz) >>
+             12;
+    v1->vy = ((long long)m->m[1][0] * v0->vx + (long long)m->m[1][1] * v0->vy +
+              (long long)m->m[1][2] * v0->vz) >>
+             12;
+    v1->vz = ((long long)m->m[2][0] * v0->vx + (long long)m->m[2][1] * v0->vy +
+              (long long)m->m[2][2] * v0->vz) >>
+             12;
+}
+
+void ApplyRotMatrix(SVECTOR* v0, VECTOR* v1) { ApplyMatrix(&M, v0, v1); }
 
 long RotTransPers(SVECTOR* v0, int* sxy, int* p, int* flag) {
     V0 = *v0;
