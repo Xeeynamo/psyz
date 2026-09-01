47c91e051ea7d9473b73cb6df3734925d12fac68
Tuomas Tynkkynen <tuomas.tynkkynen@iki.fi>
2026-09-02 02:39:24 +0300

    libcd: Decompile event.c

--- decomp/src/libcd/event.c
+++ decomp/src/libcd/event.c
@@ -1,10 +1,30 @@
 #include <common.h>
+#include <libapi.h>
 #include <libcd.h>
 
-INCLUDE_ASM("asm/nonmatchings/libcd/event", CdInit);
+void def_cbsync(void);
+void def_cbready(void);
+void def_cbread(void);
 
-INCLUDE_ASM("asm/nonmatchings/libcd/event", def_cbsync);
+int CdInit(void) {
+    int i = 4;
 
-INCLUDE_ASM("asm/nonmatchings/libcd/event", def_cbready);
+    while (CdReset(1) != 1) {
+        if (!i--) {
+            printf("CdInit: Init failed\n");
+            return 0;
+        }
+    }
 
-INCLUDE_ASM("asm/nonmatchings/libcd/event", def_cbread);
+    CdSyncCallback(def_cbsync);
+    CdReadyCallback(def_cbready);
+    CdReadCallback(def_cbread);
+    CdReadMode(0);
+    return 1;
+}
+
+void def_cbsync(void) { DeliverEvent(0xF0000003, 0x20); }
+
+void def_cbready(void) { DeliverEvent(0xF0000003, 0x40); }
+
+void def_cbread(void) { DeliverEvent(0xF0000003, 0x40); }
